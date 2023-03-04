19b01528dbe6a6957a7e56ff99d93ff28b353190
blendi <dronramone@mail.ru>
2023-03-05 03:02:54 +0600

    Добавлен вызов функции создания формы

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import tkinter as tk
-from tkinter import scrolledtext
-from utils import create_form
+import utils
 
-window = create_form()
-
-# frame_1 = tk.Frame(win, bg='red', width=300, height=250)
-# frame_2 = tk.Frame(win, bg='yellow', width=300, height=100)
-# listbox = scrolledtext.ScrolledText(frame_1, width=10, height=11)
-# entry = tk.Entry(frame_2, width=10)
-#
-# frame_1.pack(expand=True, anchor='w')
-# frame_2.pack(expand=True)
-# listbox.place(x=200, y=3)
-# entry.place(x=50, y=40)
+window = utils.create_form()
+frame_1, frame_2 = utils.create_frame(window)
+listbox = utils.create_scrolledtext(frame_1)
+entry = utils.create_entry(frame_2)
 
 tk.mainloop()
