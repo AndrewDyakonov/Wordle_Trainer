6a94c4fcad1b199703de7e4cbf169835ab348edc
blendi <dronramone@mail.ru>
2023-03-05 02:43:46 +0600

    Добавлена функция отрисовки фреймов на форме

--- utils.py
+++ utils.py
@@ -14,3 +14,18 @@ def create_form():
     win.geometry('300x350+1000+100')
     win.title('Wordle Trainer')
     win.resizable(False, False)
+    return win
+
+
+def create_frame(window):
+    """
+    Создание фреймов на форме
+    :param window: форма
+    :return:       кортеж с двумя фреймами
+    """
+    frame_1 = tk.Frame(window, bg='red', width=300, height=250)
+    frame_2 = tk.Frame(window, bg='yellow', width=300, height=100)
+
+    frame_1.pack(expand=True, anchor='w')
+    frame_2.pack(expand=True)
+    return frame_1, frame_2
