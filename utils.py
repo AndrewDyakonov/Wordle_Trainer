9cc863c88eeb97933e7c2159af635f83db1021b0
blendi <dronramone@mail.ru>
2023-03-05 02:54:49 +0600

    Добавлена функция отображения окна вывода слов

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import scrolledtext
 
 
 def create_form():
@@ -29,3 +30,15 @@ def create_frame(window):
     frame_1.pack(expand=True, anchor='w')
     frame_2.pack(expand=True)
     return frame_1, frame_2
+
+
+def create_scrolledtext(frame_1):
+    """
+    Создание окна вывода
+    :param frame_1: фрейм для отображения
+    :return: окно вывода слов
+    """
+    listbox = scrolledtext.ScrolledText(frame_1, width=10, height=11)
+
+    listbox.place(x=200, y=3)
+    return listbox
