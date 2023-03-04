c546a6e39d488817279d530cb01ed0692bdf415e
blendi <dronramone@mail.ru>
2023-03-05 02:20:40 +0600

    Создан main. Создана форма

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import scrolledtext
+
+win = tk.Tk()
+win.geometry('300x350+1000+100')
+
+frame_1 = tk.Frame(win, bg='red', width=300, height=250)
+frame_2 = tk.Frame(win, bg='yellow', width=300, height=100)
+listbox = scrolledtext.ScrolledText(frame_1, width=10, height=11)
+entry = tk.Entry(frame_2, width=10)
+
+frame_1.pack(expand=True, anchor='w')
+frame_2.pack(expand=True)
+listbox.place(x=200, y=3)
+entry.place(x=50, y=40)
+
+tk.mainloop()
